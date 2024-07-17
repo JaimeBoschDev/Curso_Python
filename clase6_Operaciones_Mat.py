e1abb37cdd4950ab7f24edace01bb1fbcefe72c8
Jaime Bosch <bombosch7@gmail.com>
2024-07-17 07:48:33 -0600

    Clase 5 Tipos de Variables, Clase 6 Operaciones Mat y bool, Clase 7 input

---
+++ clase6_Operaciones_Mat.py
@@ -0,0 +1,21 @@
+'''
+Suma: a+b
+Resta: a-b
+Multiplicación: a*b
+División: a/b
+Módulo(resto en una división): a%b
+Parte entera de división: a//b
+Potenciación: a**b
+
+
+'''
+a=10
+b=5.55
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #Modulo
+print(a//b) #Parte entera de la division
+print(a**3) #Potenciando a la 3
---
+++ clase7_Input.py
@@ -0,0 +1,7 @@
+nombre = input("Escribe tu nombre: ")
+print(nombre, type(nombre))
+
+#Input siempre regresa un String, por lo que si queremos usar numeros, debemos convertirlo a Int con casting
+
+edad = int(input("Dime tu edad: "))
+print(edad, type(edad))
