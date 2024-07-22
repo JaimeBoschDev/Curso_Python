bd92f565fe132d166c98184874bb56c23ac46eaa
Jaime Bosch <bombosch7@gmail.com>
2024-07-22 07:34:58 -0600

    Vimos diccionarios y Estructura condicional

---
+++ clase10_Diccionarios.py
@@ -0,0 +1,18 @@
+contacts = {"Jaime":{
+    "Apellido":"Gonzalez",
+    "Edad": "34"
+},
+"Damaris":{
+    "Apellido":"Barbosa",
+    "Edad": "37"
+},
+"Jose":{
+    "Apellido":"Gonzalez",
+    "Edad": "8"
+}}
+
+print(contacts)
+print(contacts.keys())
+print(contacts.values())
+print(contacts.items())
+print(contacts["Jaime"])
---
+++ clase11_If.py
@@ -0,0 +1,13 @@
+_is_Miembro = True
+age=15
+
+if _is_Miembro:
+        if age < 7:
+                print("Es miembro y menor de 7")
+        else:
+                print("Es miembro y mayor de 7")
+
+if _is_Miembro and age>7:
+        print("ES MIEMBRO Y MAYOR A 7")
+
+
