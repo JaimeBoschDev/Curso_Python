6aef51844123ec444f3949a7dfd608237a718334
Jaime Bosch <bombosch7@gmail.com>
2024-08-08 07:06:52 -0600

    Bucles

---
+++ clase12_Bubles.py
@@ -0,0 +1,22 @@
+numbers =[0,1,2,3,4,5,6,7,8,9,10]
+
+for number  in  numbers:
+    print("Numero es: ", number)
+    if number==4:
+        continue
+    print("Seguimos en => ", number)
+
+
+
+valor= 0
+
+while valor <= 10:
+    valor +=1
+    print("Valor es =>", valor)
+    if valor ==4:
+        continue
+    if valor == 6:
+        break
+    print("Tienes =>", valor)
+    
+
